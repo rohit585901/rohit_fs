3a76b035ae306c969294464e9f418e24d4d34929
rohit585901 <rohitsolanki192003@gmail.com>
2025-09-15 15:24:22 +0530

    this is commit

---
+++ Day 5/index.js
@@ -0,0 +1,34 @@
+console.log("Welcome to the FSD session");
+document.write("Welcome to the FSD session");
+document.writeln("Welcome to the FSD session");
+document.writeln("Welcome to the FSD session");
+// alert("alert message");
+console.error("error message");
+
+//input methods 
+//let a=prompt("enter value");
+
+//var
+console.log(a);
+
+var a=20;
+console.log(a);
+var a=30;
+console.log(a);
+(
+
+var a=;
+console.log(a);
+
+)
+fun(70)
+function fun(a) (
+console.log(a);
+
+
+var a=90;
+console.log(a);
+
+
+
+)
